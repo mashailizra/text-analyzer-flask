cfca891d906d51b00ce6f76337b3914722049ee5
MSI-izra <mashailizra@gmail.com>
2026-08-10 13:19:08 +0530

    refactor: introduce TextAnalyzer class with custom file-validation exceptions

--- analyzer.py
+++ analyzer.py
@@ -3,6 +3,7 @@ from collections import Counter
 
 class UnsupportedFileTypeError(Exception):
     pass
+
 class EmptyFileError(Exception):
     pass
 
@@ -21,63 +22,71 @@ class TextAnalyzer:
         with open(self.file_path, "r") as file:
             for line in file:
                 yield line
+    @property
+    def word_count(self):
+        count=0
 
-analyzer = TextAnalyzer("sample.txt")
-for line in analyzer.read_lines():
-    print(line, end="")
+        for line in self.read_lines():
+            count+=len(line.split())
 
-print("\nText Analysis Toolkit")
-file_path =input("Enter the path of the text file:")
+        return count
+
+    @property
+    def char_count(self):
+        count=0
+
+        for line in self.read_lines():
+            line=line.rstrip("\n")
+            count+=len(line)
+
+        return count
+
+    @property
+    def sentence_count(self):
+        count=0
+
+        for line in self.read_lines():
+            count+=line.count(".")
+            count+=line.count("!")
+            count+=line.count("?")
+
+        return count
+
+    def top_words(self,n=10):
+        words=[]
+
+        stopwords ={"the","a","an","is","and","to","of","in"}
+
+        for line in self.read_lines():
+            line=line.lower()
+            line=line.replace(".","")
+            line=line.replace("!","")
+            line=line.replace("?","")
+
+            words.extend(line.split())
+
+        filtered_words=filter(lambda word:word not in stopwords,words)
+        word_counter=Counter(filtered_words)
+
+        return word_counter.most_common(n)
+
+    
+print("Text Analysis Toolkit")
+
+file_path = input("Enter the path of the text file: ")
+
+try:
+    analyzer = TextAnalyzer(file_path)
+
+    print("Word count:", analyzer.word_count)
+    print("Character count:", analyzer.char_count)
+    print("Sentence count:", analyzer.sentence_count)
+    print("Top words:", analyzer.top_words())
+
+except UnsupportedFileTypeError:
+    print("Error: Only .txt files are supported.")
+
+except EmptyFileError:
+    print("Error: The file is empty.")
 
 
-def read_lines(file_path):
-    with open(file_path,"r")as file:
-        for line in file:
-            yield line
-
-word_count=0
-character_count=0
-character_count_no_spaces=0
-line_count=0
-sentence_count=0
-
-words =[]
-
-stopwords = {"the","a","an","is","and","to","of","in"}
-
-
-for line in read_lines(file_path):
-    line=line.rstrip("\n")
-
-    clean_line= line.lower()
-    clean_line= clean_line.replace(".","")
-    clean_line= clean_line.replace("!","")
-    clean_line= clean_line.replace("?","")
-
-    line_words = clean_line.split()
-    words.extend(line_words)
-
-    line_count += 1
-    word_count += len(line.split())
-    character_count += len(line)
-    character_count_no_spaces += len(line.replace(" ",""))
-    sentence_count += line.count(".")
-    sentence_count += line.count("!")
-    sentence_count += line.count("?")
-
-word_counter =Counter(words)
-
-filtered_words= filter(lambda word: word not in stopwords,words)
-filtered_counter= Counter(filtered_words)
-
-
-print("Word count:",word_count)
-print("Character count:",character_count)
-print("Character count without spaces:",character_count_no_spaces)
-print("Line count:",line_count)
-print("sentence count:",sentence_count)
-
-print("\nTop 10 most commomn words:")
-
-for word,count in filtered_counter.most_common(10):
-    print(word,":",count)
